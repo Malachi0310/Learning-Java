4368b62f4f242eb8382384d7277042ab47bcf0fd
Malachi Narsiah <narsiahmalachi07@gmail.com>
2026-05-13 18:23:37 +0200

    Update Driver_earnings.java

--- Driver_earnings.java
+++ Driver_earnings.java
@@ -3,9 +3,9 @@ class Driver_earnings{
 
         Trip[] trips = new Trip[3];
         
-        trips[0] = new Trip("John Doe", "Sam Smith", 12.0, 10.0);
+        trips[0] = new Trip("John Doe", "Sam Smith", 12.0, 30.0);
         trips[1] = new Trip("Denver Adams", "James Micheal", 3.0, 10.0);
-        trips[2] = new Trip("Connor Tels", "Adam Watkins", 6, 10.0);        
+        trips[2] = new Trip("Connor Tels", "Adam Watkins", 30, 20.0);        
         double total_earnings  = 0.0;
         double highest_fare = 0.0;
         
@@ -14,16 +14,25 @@ class Driver_earnings{
             trip.displayTrip();
             double fare = trip.calculateFare();
             
+            
             total_earnings += fare;
             
             if (fare > highest_fare){
                 highest_fare = fare;
             }
             
+            if (fare >= 500){
+                System.out.println("Premium");
+            } else if (fare  >= 200 && fare < 500 ){
+                System.out.println("Standard");
+            } else {
+                System.out.println("Budget");
+            }
+            
         }
-        System.out.println("\nDriver Summary:")
+        System.out.println("\nDriver Summary:");
         System.out.println("Total Earnings: " + total_earnings);
-        System.out.
+        System.out.println("Highest fare: " + highest_fare);
         
     }
 }
